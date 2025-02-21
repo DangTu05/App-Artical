0fb19fc9434c2c23a32327ed4b62434737363409
Dang Tu <dtu161205@gmail.com>
2025-02-21 15:35:17 +0700

    Add dist

--- dist/config/database.config.js
+++ dist/config/database.config.js
@@ -12,24 +12,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const database_config_1 = require("./config/database.config");
-const environment_config_1 = __importDefault(require("./config/environment.config"));
-const app = (0, express_1.default)();
-const port = parseInt(environment_config_1.default.PORT);
-const startServer = () => {
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
+exports.connect = connect;
+const mongoose_1 = __importDefault(require("mongoose"));
+const environment_config_1 = __importDefault(require("../config/environment.config"));
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(environment_config_1.default.MONGO_URL);
+            console.log("connect sucessfully!!!");
+        }
+        catch (_a) {
+            console.log("connect failue!!!");
+        }
     });
-};
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        console.log("Connecting to database...");
-        yield (0, database_config_1.connect)();
-        startServer();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-}))();
+}
