de557058f1ec6685970990522b395bc69252610f
Dang Tu <dtu161205@gmail.com>
2025-02-21 17:57:28 +0700

    Cấu hình graphql

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
-import express, { Express, Request, Response } from "express";
+import express, { Application, Express, Request, Response } from "express";
 import { connect } from "./config/database.config";
 import env from "./config/environment.config";
-const app: Express = express();
+import { ApolloServer} from "apollo-server-express";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+const app = express() as any;
 const port: number = parseInt(env.PORT as string);
 const startServer = (): void => {
   app.listen(port, () => {
@@ -11,7 +14,13 @@ const startServer = (): void => {
 };
 /// anonymous async function(IIFE)
 (async (): Promise<void> => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   try {
+    ///graphQl
+    const apolloServer = new ApolloServer({ typeDefs, resolvers });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app: app, path: "/graphql" });
     console.log("Connecting to database...");
     /// Kết nối db, chỉ khi thành công mới chạy server
     await connect();
